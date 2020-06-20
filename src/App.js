dd874306511193bfdc637860d1acee47225b7de1
yhussin <yhussin@gmail.com>
2020-06-20 15:05:44 -0700

    Adds routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
+import { Route, Switch } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import ProductList from './components/ProductList'
+import Details from './components/Details'
+import Default from './components/Default'
+import Cart from './components/Cart'
+
 
 function App() {
   return (
     <>
-    <Navbar />
-    <div>
-      <h1>Hello</h1>
-    </div>
+      <Navbar />
+      <Switch>
+        < Route exact path="/" component={ProductList} />
+        < Route path="/cart" component={Cart} />
+        < Route path="/details" component={Details} />
+        < Route component={Default} />
+      </Switch>
     </>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class Navbar extends Component {
     render() {
         return (
             <nav className="navbar bg-dark">
-                <a href="www.google.com">Links</a>
+                <Link to="/">Home</Link>
+                <Link to="/details">Details</Link>
+                <Link to="/cart">Cart</Link>
             </nav>
         )
     }
