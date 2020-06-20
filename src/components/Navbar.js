933691f3cfb08302d5151c3a6d5d2804141690a8
yhussin <yhussin@gmail.com>
2020-06-19 21:56:30 -0700

    Adds initial files

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Navbar extends Component {
+    render() {
+        return (
+            <nav className="navbar bg-dark">
+                <a href="www.google.com">Links</a>
+            </nav>
+        )
+    }
+}
