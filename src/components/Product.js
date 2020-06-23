6775e19d7cb3abd2713109288fd6abd6152e98d5
yhussin <yhussin@gmail.com>
2020-06-23 12:39:29 -0700

    ProductList render

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
+import { ProductConsumer } from '../context'
+import { Link } from 'react-router-dom'
 
 export default class Product extends Component {
     render() {
+        const { id, title, img, price, inCart } = this.props.product;
+
         return (
-            <div>
-                <h1>Product Component</h1>
+            <div className="container">
+                <h1>{title}</h1>
+                <div onClick={() => {
+                    console.log("clicked image")
+                }} >{img}</div>
+                <Link to="/details">
+                    <img src={img} alt="product" className="image"></img>
+                </Link>
+                <button className="cart-btn" disabled={inCart}></button>
             </div>
         )
     }
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,30 +1,29 @@
 import React, { Component } from 'react'
 import Product from './Product'
 import Title from './Title'
-import {ProductConsumer} from '../context'
-import { storeProducts } from '../data'
+import { ProductConsumer } from '../context'
 
 export default class ProductList extends Component {
-    state = {
-        products: storeProducts
-    }
+
 
     render() {
         return (
             <div className="container">
                 <div className="row">
-                  <Title name="Our" title="Products" />
+                    <Title name="Our" title="Products" />
                 </div>
                 <div>
                     <ProductConsumer>
                         {value => {
-                          return value.products.map((product) => {
-                              return <Product key={product.id} product={product}/>
-                          })
+                            return value.products.map((product) => {
+                                return <Product key={product.id}
+                                    product={product}
+                                />
+                            })
                         }}
                     </ProductConsumer>
                 </div>
-            </div> 
+            </div>
             // < Product /> 
         )
     }
