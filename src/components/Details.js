0e62e14d10f3b29c9f51043e37d943f1d39ba54b
yhussin <yhussin@gmail.com>
2020-06-23 22:29:26 -0700

    Details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
+import {ProductConsumer} from '../context'
+import {Link} from 'react-router-dom'
 
 export default class Details extends Component {
     render() {
         return (
+<ProductConsumer>
+    {value => {
+        const {id, company, img, info, price, title, inCart } = value.detailProduct
+        return (
+            <>
             <div>
-                <h1>Hello from Details</h1>
+                <p>Model: {title}</p>
+                <img src={img} alt="product"></img>
+                {company}
             </div>
+            <Link to="/">
+                <button>Back to Home</button></Link>
+            <button
+            disabled={inCart}
+            onClick={() => {
+                value.addToCart(id)
+            }}
+            >{inCart ? "inCart" : "Add to Cart"}</button>
+            </>
+        )
+    }}
+</ProductConsumer>
         )
     }
 }
