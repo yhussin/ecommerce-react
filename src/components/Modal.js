e602eefaff92c989a5f14533755f3a77f9ac0e98
yhussin <yhussin@gmail.com>
2020-06-24 16:57:52 -0700

    Modal on item selection

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react'
+import { ProductConsumer } from '../context'
+import { Link } from 'react-router-dom'
+import '../App.css'
 
 export default class Modal extends Component {
     render() {
         return (
-            <div>
-                <h1>Hello from modal</h1>
-            </div>
+            <ProductConsumer>
+                {(value) => {
+                    const { modalOpen, closeModal } = value;
+                    const { img, title, price } = value.modalProduct
+
+                    if (!modalOpen) {
+                        return null;
+                    }
+                    else {
+                        return (
+                            <div className="modal">
+                            
+                                    <h1>Item added to cart</h1>
+                                    <img src={img} alt="product"></img>
+                                    {title}
+                                    <p>${price}</p>
+                                    <Link to="/" onClick={() => closeModal()}>back to home</Link>
+                                    <Link to="/cart" onClick={() => closeModal()}>go to cart</Link>
+                  
+                            </div>
+                        )
+                    }
+
+                }}
+            </ProductConsumer>
         )
     }
 }
+
